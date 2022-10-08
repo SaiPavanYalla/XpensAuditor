439704650834f07d4fb564df3408653ff23981b7
vineethdasi <112347129+vineethdasi@users.noreply.github.com>
2022-10-08 16:05:03 -0400

    ContactUs cleanup

--- app/src/main/java/com/xa/xpensauditor/ContactUs.java
+++ app/src/main/java/com/xa/xpensauditor/ContactUs.java
@@ -18,8 +18,6 @@ public class ContactUs extends AppCompatActivity implements View.OnClickListener
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
 
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
@@ -27,76 +25,11 @@ public class ContactUs extends AppCompatActivity implements View.OnClickListener
             actionBar.setDisplayHomeAsUpEnabled(false); // remove the left caret
             actionBar.setDisplayShowHomeEnabled(false); // remove the icon
         }
-
-
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
-
-
-//        SMS = (Button) findViewById(R.id.sendSMS);
-//        Call = (Button)findViewById(R.id.call);
-//        Email = (Button)findViewById(R.id.sendEmail);
-
-//        SMS.setOnClickListener(this);
-//        Call.setOnClickListener(this);
-//        Email.setOnClickListener(this);
     }
-
 
     @Override
     public void onClick(View view) {
-//        switch (view.getId()) {
-////            case R.id.sendSMS: {
-////
-////                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:" + "9790084862"));
-////                intent.putExtra("sms_body", "Test Message");
-////                startActivity(intent);
-////
-////            }
-////            break;
-//
-//            case R.id.sendEmail: {
-//
-//
-//                String[] TO = {"13pramo@gmail.com"};
-//                Intent emailIntent = new Intent(Intent.ACTION_SEND);
-//                emailIntent.setData(Uri.parse("mailto:"));
-//                emailIntent.setType("text/plain");
-//
-//
-//                emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
-//                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Some subject");
-//                emailIntent.putExtra(Intent.EXTRA_TEXT, "Test message");
-//
-//                try {
-//                    startActivity(Intent.createChooser(emailIntent, "Send mail..."));
-//                    finish();
-//                    Log.i("Finished sending email", "");
-//                } catch (android.content.ActivityNotFoundException ex) {
-//                    Toast.makeText(this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
-//                }
-//
-//
-//            }
-//            break;
-//
-////            case R.id.call: {
-////
-////                Intent callIntent = new Intent(Intent.ACTION_CALL);
-////                callIntent.setData(Uri.parse("tel:9491699997"));
-////
-////                if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-////                    return;
-////                }
-////                startActivity(callIntent);
-////
-////            }
-////            break;
-//        }
     }
-
-
 
     private void sendmail(String mail) {
         String mailto = "mailto:"+mail +
@@ -113,31 +46,24 @@ public class ContactUs extends AppCompatActivity implements View.OnClickListener
         }
     }
 
-
     public void SendMailvineeth(View view) {
         sendmail("vineethdasi22@gmail.com");
     }
 
-
-
     public void SendMailsunandini(View view) {
         sendmail("sunandini.medisetti@gmail.com");
-
     }
 
     public void SendMailsahiti(View view) {
         sendmail("sahithi.ammana@gmail.com");
-
     }
 
     public void SendMailpavan(View view) {
         sendmail("ysaipavan99@gmail.com");
-
     }
 
     public void SendMailmithila(View view) {
         sendmail("mithilareddy1999@gmail.com");
-
     }
 
     public void callSupport(View view) {
