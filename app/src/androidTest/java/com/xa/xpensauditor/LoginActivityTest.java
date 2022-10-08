4a99f3d97e6787edb3d8855d8524848e2314e586
vineethdasi <112347129+vineethdasi@users.noreply.github.com>
2022-10-08 14:54:55 -0400

    updated testcases & gradle

--- app/src/androidTest/java/com/xa/xpensauditor/LoginActivityTest.java
+++ app/src/androidTest/java/com/xa/xpensauditor/LoginActivityTest.java
@@ -1,93 +1,159 @@
 package com.xa.xpensauditor;
 
+
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
-
-import android.app.Activity;
-import android.content.Context;
-
-import androidx.appcompat.widget.Toolbar;
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.espresso.PerformException;
-import androidx.test.espresso.UiController;
-import androidx.test.espresso.ViewAction;
-import androidx.test.espresso.ViewInteraction;
-import androidx.test.espresso.util.HumanReadables;
-import androidx.test.espresso.util.TreeIterables;
-import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import org.hamcrest.Matcher;
-import org.hamcrest.core.IsInstanceOf;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
 import static org.hamcrest.Matchers.allOf;
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
 
-
-import static org.junit.Assert.assertEquals;
-
-import android.content.Context;
 import android.os.SystemClock;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.filters.LargeTest;
 
-import com.firebase.client.Firebase;
-import com.google.firebase.auth.FirebaseAuth;
-
-import junit.framework.TestCase;
-
-import org.junit.After;
-import org.junit.Before;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class LoginActivityTest {
 
-
-@RunWith(AndroidJUnit4ClassRunner.class)
-public class LoginActivityTest extends TestCase {
-
+    @Rule
+    public ActivityScenarioRule<LoginActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(LoginActivity.class);
 
     @Test
-    public void testLogin(){
+    public void loginActivityTest() {
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.email),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.google.android.material.textfield.TextInputLayout")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatEditText.perform(replaceText("vineethdasi22@gmail.com"), closeSoftKeyboard());
 
-        if(true)
-        return;
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.password),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.google.android.material.textfield.TextInputLayout")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("123456"), closeSoftKeyboard());
 
-        ActivityScenario activityScenario=ActivityScenario.launch(LoginActivity.class);
-        onView(withId(R.id.email)).check(matches(isDisplayed()));
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.btn_login), withText("LOGIN"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                        0),
+                                3),
+                        isDisplayed()));
+        materialButton.perform(click());
 
-        onView(withId(R.id.email)).perform(typeText("vineethdasi22@gmail.com"));
-        onView(withId(R.id.password)).perform(typeText("123456"));
-        onView(withId(R.id.btn_login)).perform(click());
+        SystemClock.sleep(5000);
 
-        SystemClock.sleep(10000);
+        ViewInteraction imageView = onView(
+                allOf(withContentDescription("More options"),
+                        withParent(withParent(withId(R.id.toolbar))),
+                        isDisplayed()));
+        imageView.check(matches(isDisplayed()));
 
+        ViewInteraction imageButton2 = onView(
+                allOf(withContentDescription("Open navigation drawer"),
+                        withParent(allOf(withId(R.id.toolbar),
+                                withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class)))),
+                        isDisplayed()));
+        imageButton2.check(matches(isDisplayed()));
 
-        ViewInteraction textView = onView(allOf(withText("XpensAuditor"), withParent(allOf(withId(R.id.toolbar), withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class)))), isDisplayed()));
-        textView.check(matches(withText("XpensAuditor")));
+        ViewInteraction overflowMenuButton = onView(
+                allOf(withContentDescription("More options"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.toolbar),
+                                        2),
+                                0),
+                        isDisplayed()));
+        overflowMenuButton.perform(click());
 
+        SystemClock.sleep(1000);
+        ViewInteraction materialTextView = onView(
+                allOf(withId(androidx.recyclerview.R.id.title), withText("Account Settings"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(androidx.constraintlayout.widget.R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        materialTextView.perform(click());
+        SystemClock.sleep(1000);
 
+        ViewInteraction materialButton4 = onView(
+                allOf(withId(R.id.sign_out), withText("Sign Out"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                        1),
+                                12),
+                        isDisplayed()));
+        materialButton4.perform(click());
+        SystemClock.sleep(1000);
+        ViewInteraction textView2 = onView(
+                allOf(withId(android.R.id.message), withText("Do you Really want to SignOut ?"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class))),
+                        isDisplayed()));
+        textView2.check(matches(withText("Do you Really want to SignOut ?")));
+
+        ViewInteraction materialButton5 = onView(
+                allOf(withId(android.R.id.button1), withText("SignOut"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                3)));
+        materialButton5.perform(scrollTo(), click());
+        SystemClock.sleep(1000);
     }
 
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
 
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
 }
