9ae21a0cabdb8805eeb4b1635245370fed5afb28
vineethdasi <112347129+vineethdasi@users.noreply.github.com>
2022-10-09 14:01:13 -0400

    Update SuggestActivityTest.java

--- app/src/androidTest/java/com/xa/xpensauditor/SuggestActivityTest.java
+++ app/src/androidTest/java/com/xa/xpensauditor/SuggestActivityTest.java
@@ -5,7 +5,6 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
-import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
@@ -26,10 +25,12 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
+import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -42,8 +43,20 @@ public class SuggestActivityTest {
     public ActivityScenarioRule<LoginActivity> mActivityScenarioRule =
             new ActivityScenarioRule<>(LoginActivity.class);
 
+    @After
+    public void signoutandclear()
+    {
+        FirebaseAuth auth;
+        auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            auth.signOut();
+        }
+    }
+
     @Test
-    public void suggestActivityTest() {
+    public void suggestActivityTest2() {
+        SystemClock.sleep(2000);
+
         ViewInteraction appCompatEditText = onView(
                 allOf(withId(R.id.email),
                         childAtPosition(
@@ -52,8 +65,9 @@ public class SuggestActivityTest {
                                         0),
                                 0),
                         isDisplayed()));
-        appCompatEditText.perform(replaceText("mithilareddy1999@gmail.com"), closeSoftKeyboard());
-        SystemClock.sleep(1000);
+        appCompatEditText.perform(replaceText("vineethdasi22@gmail.com"), closeSoftKeyboard());
+        SystemClock.sleep(2000);
+
         ViewInteraction appCompatEditText2 = onView(
                 allOf(withId(R.id.password),
                         childAtPosition(
@@ -62,8 +76,10 @@ public class SuggestActivityTest {
                                         0),
                                 0),
                         isDisplayed()));
-        appCompatEditText2.perform(replaceText("defaultpw9"), closeSoftKeyboard());
-        SystemClock.sleep(1000);
+        appCompatEditText2.perform(replaceText("123456"), closeSoftKeyboard());
+        SystemClock.sleep(2000);
+
+
         ViewInteraction materialButton = onView(
                 allOf(withId(R.id.btn_login), withText("LOGIN"),
                         childAtPosition(
@@ -73,14 +89,9 @@ public class SuggestActivityTest {
                                 3),
                         isDisplayed()));
         materialButton.perform(click());
-        SystemClock.sleep(3000);
-        ViewInteraction imageButton = onView(
-                allOf(withContentDescription("Open navigation drawer"),
-                        withParent(allOf(withId(R.id.toolbar),
-                                withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class)))),
-                        isDisplayed()));
-        imageButton.check(matches(isDisplayed()));
-        SystemClock.sleep(1000);
+        SystemClock.sleep(2000);
+
+
         ViewInteraction appCompatImageButton = onView(
                 allOf(withContentDescription("Open navigation drawer"),
                         childAtPosition(
@@ -91,14 +102,9 @@ public class SuggestActivityTest {
                                 1),
                         isDisplayed()));
         appCompatImageButton.perform(click());
-        SystemClock.sleep(1000);
-        ViewInteraction checkedTextView = onView(
-                allOf(withId(com.google.android.material.R.id.design_menu_item_text), withText("Suggestions"),
-                        withParent(allOf(withId(R.id.nav_suggest),
-                                withParent(withId(com.google.android.material.R.id.design_navigation_view)))),
-                        isDisplayed()));
-        checkedTextView.check(matches(isDisplayed()));
-        SystemClock.sleep(1000);
+        SystemClock.sleep(2000);
+
+
         ViewInteraction navigationMenuItemView = onView(
                 allOf(withId(R.id.nav_suggest),
                         childAtPosition(
@@ -109,13 +115,10 @@ public class SuggestActivityTest {
                                 13),
                         isDisplayed()));
         navigationMenuItemView.perform(click());
-        SystemClock.sleep(1000);
-        ViewInteraction button = onView(
-                allOf(withId(R.id.button3), withText("DONE"),
-                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class))),
-                        isDisplayed()));
-        button.check(matches(isDisplayed()));
-        SystemClock.sleep(1000);
+
+        SystemClock.sleep(2000);
+
+
         ViewInteraction appCompatEditText3 = onView(
                 allOf(withId(R.id.editText5),
                         childAtPosition(
@@ -126,6 +129,8 @@ public class SuggestActivityTest {
                         isDisplayed()));
         appCompatEditText3.perform(replaceText("good"), closeSoftKeyboard());
         SystemClock.sleep(2000);
+
+
         ViewInteraction materialButton2 = onView(
                 allOf(withId(R.id.button3), withText("DONE"),
                         childAtPosition(
@@ -135,58 +140,14 @@ public class SuggestActivityTest {
                                 2),
                         isDisplayed()));
         materialButton2.perform(click());
-        SystemClock.sleep(1000);
-        ViewInteraction textView = onView(
-                allOf(withText("XpensAuditor"),
-                        withParent(allOf(withId(R.id.toolbar),
-                                withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class)))),
+        SystemClock.sleep(2000);
+
+
+        ViewInteraction imageButton = onView(
+                allOf(withId(R.id.fab),
+                        withParent(withParent(withId(R.id.drawer_layout))),
                         isDisplayed()));
-        textView.check(matches(withText("XpensAuditor")));
-        SystemClock.sleep(1000);
-        ViewInteraction overflowMenuButton = onView(
-                allOf(withContentDescription("More options"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.toolbar),
-                                        2),
-                                0),
-                        isDisplayed()));
-        overflowMenuButton.perform(click());
-        SystemClock.sleep(1000);
-        ViewInteraction materialTextView = onView(
-                allOf(withId(androidx.recyclerview.R.id.title), withText("Account Settings"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(androidx.constraintlayout.widget.R.id.content),
-                                        0),
-                                0),
-                        isDisplayed()));
-        materialTextView.perform(click());
-        SystemClock.sleep(1000);
-        ViewInteraction materialButton3 = onView(
-                allOf(withId(R.id.sign_out), withText("Sign Out"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
-                                        1),
-                                12),
-                        isDisplayed()));
-        materialButton3.perform(click());
-        SystemClock.sleep(1000);
-        ViewInteraction materialButton4 = onView(
-                allOf(withId(android.R.id.button1), withText("SignOut"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.ScrollView")),
-                                        0),
-                                3)));
-        materialButton4.perform(scrollTo(), click());
-        SystemClock.sleep(1000);
-        ViewInteraction imageView = onView(
-                allOf(withContentDescription("XpensAuditor"),
-                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class))),
-                        isDisplayed()));
-        imageView.check(matches(isDisplayed()));
+        imageButton.check(matches(isDisplayed()));
     }
 
     private static Matcher<View> childAtPosition(
