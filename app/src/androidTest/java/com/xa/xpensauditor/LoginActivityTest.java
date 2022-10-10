0e3cde3d996647cf4dc73bf999edb447bf36973c
vineethdasi <112347129+vineethdasi@users.noreply.github.com>
2022-10-09 20:44:58 -0400

    Update LoginActivityTest.java

--- app/src/androidTest/java/com/xa/xpensauditor/LoginActivityTest.java
+++ app/src/androidTest/java/com/xa/xpensauditor/LoginActivityTest.java
@@ -67,7 +67,7 @@ public class LoginActivityTest {
                                         0),
                                 0),
                         isDisplayed()));
-        appCompatEditText.perform(replaceText("vineethdasi22@gmail.com"), closeSoftKeyboard());
+        appCompatEditText.perform(replaceText("xpensauditor@gmail.com"), closeSoftKeyboard());
 
         SystemClock.sleep(2000);
 
@@ -79,7 +79,7 @@ public class LoginActivityTest {
                                         0),
                                 0),
                         isDisplayed()));
-        appCompatEditText2.perform(replaceText("123456"), closeSoftKeyboard());
+        appCompatEditText2.perform(replaceText("defaultpw9"), closeSoftKeyboard());
         SystemClock.sleep(2000);
 
         ViewInteraction materialButton = onView(
