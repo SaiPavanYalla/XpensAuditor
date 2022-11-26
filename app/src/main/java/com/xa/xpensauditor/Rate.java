f74da4212273810436fae61263d07a57d03a6143
kajadhav <kajadhav@ncsu.edu>
2022-11-26 11:27:24 -0500

    feat: store rating in db and prompt to overwrite on nee sub

--- app/src/main/java/com/xa/xpensauditor/Rate.java
+++ app/src/main/java/com/xa/xpensauditor/Rate.java
@@ -1,11 +1,16 @@
 package com.xa.xpensauditor;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.RatingBar;
 import android.widget.Toast;
+
+import com.firebase.client.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Rate extends AppCompatActivity {
 
@@ -15,16 +20,37 @@ public class Rate extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+        Firebase mRootRef, RefUid, RefRating;
+        FirebaseAuth auth= FirebaseAuth.getInstance();
+        mRootRef = new Firebase("https://xpensauditor-g11-default-rtdb.firebaseio.com/");
+        mRootRef.keepSynced(true);
+        String Uid = auth.getUid();
+        RefUid = mRootRef.child(Uid);
+        RefRating = RefUid.child("Rating");
 
         r=(RatingBar)findViewById(R.id.ratingBar);
         r.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
 
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
 
-                if (fromUser == true) {
-
+                if(RefRating != null && fromUser == true){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(Rate.this);
+                    builder.setMessage("You already submitted a rating. Rate Again ?");
+                    builder.setTitle("Already Rated!");
+                    builder.setCancelable(false);
+                    builder.setPositiveButton("Yes", (DialogInterface.OnClickListener) (dialog, which) -> {
+                        Toast.makeText(getApplicationContext(), String.valueOf(ratingBar.getRating()), Toast.LENGTH_LONG).show();
+                        RefRating.setValue(String.valueOf(ratingBar.getRating()));
+                    });
+                    builder.setNegativeButton("No", (DialogInterface.OnClickListener) (dialog, which) -> {
+                        dialog.cancel();
+                    });
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+                }
+                else if (fromUser == true) {
                     Toast.makeText(getApplicationContext(), String.valueOf(ratingBar.getRating()), Toast.LENGTH_LONG).show();
+                    RefRating.setValue(String.valueOf(ratingBar.getRating()));
                 }
             }
         });
