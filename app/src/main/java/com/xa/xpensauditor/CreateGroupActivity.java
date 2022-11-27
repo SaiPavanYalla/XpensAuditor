1e59536831e6c8c9e6bbae931b653f8e25bb02b0
arnab <arnab.prophet@gmail.com>
2022-11-27 14:45:04 -0500

    Groups are now being created

---
+++ app/src/main/java/com/xa/xpensauditor/CreateGroupActivity.java
@@ -0,0 +1,138 @@
+package com.xa.xpensauditor;
+
+import androidx.annotation.AnyRes;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+
+import com.firebase.client.Firebase;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.Objects;
+
+public class CreateGroupActivity extends AppCompatActivity {
+
+    private TextInputEditText inputGroupName;
+    private TextInputEditText inputUserEmailAddedToGroup;
+
+
+    private Firebase mRootRef;
+    private Firebase RefNewGroup;
+    private DatabaseReference RefNewUserEmailAddedToGroup;
+    private Firebase RefNewUserEmailAddedToNewGroup;
+    private Firebase RefCat,RefFood,RefHealth,RefTravel,RefEdu,RefBills,RefHomeNeeds,RefOthers;
+
+    private Button btnAddUser;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        Firebase.setAndroidContext(this);
+        //Get Firebase auth instance
+        mRootRef = new Firebase("https://xpense-auditor-default-rtdb.firebaseio.com");
+
+        btnAddUser = (Button) findViewById(R.id.sign_in_button); //change id to add user button
+        inputUserEmailAddedToGroup = (TextInputEditText) findViewById(R.id.email);
+        inputGroupName = (TextInputEditText) findViewById(R.id.Name);
+
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        // Add user email to group
+        btnAddUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String userEmailAddedToGroup = inputUserEmailAddedToGroup.getText().toString().trim();
+                String groupName = inputGroupName.getText().toString().trim();
+
+
+                if (TextUtils.isEmpty(userEmailAddedToGroup)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                DatabaseReference mDatabase= FirebaseDatabase.getInstance().getReference();
+
+                mDatabase.get().addOnCompleteListener(new OnCompleteListener<com.google.firebase.database.DataSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<com.google.firebase.database.DataSnapshot> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(CreateGroupActivity.this, Objects.requireNonNull(task.getException()).toString(), Toast.LENGTH_LONG).show();
+                        }
+                        else {
+                            boolean entryAdded=false;
+                            for (com.google.firebase.database.DataSnapshot databaseEntry:task.getResult().getChildren()) {
+                                if(databaseEntry.child("Group Name").exists() && databaseEntry.child("Group Name").equals(groupName))
+                                {
+                                    String key = databaseEntry.getKey();
+                                    RefNewUserEmailAddedToGroup = mDatabase.child(key).push();
+                                    RefNewUserEmailAddedToGroup.setValue(userEmailAddedToGroup);
+                                    entryAdded=true;
+                                    break;
+                                }
+                            }
+                            if (!entryAdded)
+                            {
+                                RefNewGroup = mRootRef.push();
+                                RefNewGroup.child("Group Name").setValue(groupName);
+                                RefNewUserEmailAddedToNewGroup = RefNewGroup.push();
+                                RefNewUserEmailAddedToNewGroup.setValue(userEmailAddedToGroup);
+                                RefCat=RefNewGroup.child("Categories");
+                                RefFood=RefCat.child("Food");
+                                RefFood.setValue("");
+                                RefHealth=RefCat.child("Health");
+                                RefHealth.setValue("");
+                                RefTravel=RefCat.child("Travel");
+                                RefTravel.setValue("");
+                                RefEdu=RefCat.child("Education");
+                                RefEdu.setValue("");
+                                RefBills=RefCat.child("Bills");
+                                RefBills.setValue("");
+                                RefHomeNeeds=RefCat.child("Home Needs");
+                                RefHomeNeeds.setValue("");
+                                RefOthers=RefCat.child("Others");
+                                RefOthers.setValue("");
+                            }
+                        }
+                    }
+
+                });
+
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
+    }
+
+}
