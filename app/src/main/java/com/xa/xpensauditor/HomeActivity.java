b7654bb5edca13f45869ce8febb4c7091caaf3aa
Harshitha <harshitha.paruchurii@gmail.com>
2022-12-01 12:19:03 -0500

    updated Homeactivity

--- app/src/main/java/com/xa/xpensauditor/HomeActivity.java
+++ app/src/main/java/com/xa/xpensauditor/HomeActivity.java
@@ -280,7 +280,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         i++;
         i++;
         i++;
-      //  i++;
+        i++;
         i++;
         i++;
         i++;
